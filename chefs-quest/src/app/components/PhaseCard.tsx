aecedeb1fb99466d81ead434f3f694c5bd64f809
AlexLPinheiro <alexlp2k6@gmail.com>
2026-04-08 16:58:32 -0300

    feat: sla

---
+++ chefs-quest/src/app/components/PhaseCard.tsx
@@ -0,0 +1,7 @@
+export default function PhaseCard(){
+    return(
+        <div className="bg-white w-90 h-60 rounded-2xl shadow-md p-4 text-black">
+            <h1>Portugues</h1>
+        </div>
+    )
+}
---
+++ chefs-quest/src/app/page.tsx
@@ -0,0 +1,12 @@
+import PhaseCard from "./components/PhaseCard";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full p-5 flex justify-center">
+      <PhaseCard>
+
+      </PhaseCard>
+    </div>
+
+  )
+}
